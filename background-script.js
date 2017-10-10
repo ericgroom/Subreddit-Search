96fd9fc5b20659d6263eedde6feead0dfa4cc3c7
Eric Groom <bcgroom@gmail.com>
2017-10-10 14:05:19 -0700

    add basic sorting and time filtering w/o validation

--- background-script.js
+++ background-script.js
@@ -1,3 +1,6 @@
+const SORT = ["relevance", "new", "top", "comments"]
+const TIME = ["all", "day", "week", "month", "year"]
+
 browser.omnibox.setDefaultSuggestion({
   description: `Search a specific subreddit for a query`
 })
@@ -25,8 +28,21 @@ browser.omnibox.onInputEntered.addListener((text, disposition) => {
 const BuildURL = (rawText) => {
   let args = rawText.split(" ")
   let subreddit = args.shift()
-  let query = args.join('+')
-  let url = `https://www.reddit.com/r/${subreddit}/search?q=${query}&restrict_sr=on&sort=relevance&t=all`
+  let params = args.filter(token => token.includes(':'))
+                   .map(param => param.split(':'))
+                   .reduce((obj, param) => { // reduce to single object
+                      obj[param[0]] = param[1]
+                      return obj
+                    }, {})
+  console.log(params)
+  let query = args.filter(arg => !arg.includes(':')).join('+')
+  let url = `https://www.reddit.com/r/${subreddit}/search?q=${query}&restrict_sr=on`
+  if (params.sort) {
+    url += `&sort=${params.sort}`
+  }
+  if (params.time) {
+    url += `&t=${params.time}`
+  }
   return url
 }
 
